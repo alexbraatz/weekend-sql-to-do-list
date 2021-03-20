8d863737beea935385ecacea51e92775f4b7994e
alex braatz <alexjbraatz@gmail.com>
2021-03-20 16:00:15 -0500

    POST route succesfully taking dynamic data and posting to DB

--- server/modules/routes/task_route.js
+++ server/modules/routes/task_route.js
@@ -14,7 +14,19 @@ tasksRouter.get( '/', ( req, res )=>{
 })
 
 tasksRouter.post( '/', ( req, res )=>{
+    // req.body is the new task we'll be adding to the DB
     console.log( 'add new task POST route:', req.body );
+    // create queryString
+    let queryString = `INSERT INTO "tasks" ( "newTask", complete, delete ) VALUES ( $1, $2, $3 )`;
+
+    pool.query( queryString, [ req.body.newTask, req.body.complete, req.body.delete ] ).then( ( results )=>{
+        // if successful send back OK code
+        res.sendStatus( 200 );
+    }).catch( ( error )=>{
+        console.log( error );
+        res.sendStatus( 500 );
+    })
+
 })
 
 module.exports = tasksRouter;
