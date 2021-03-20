74ef165b7802ef5b34ba5c3cbe5596b5baa4f677
alex braatz <alexjbraatz@gmail.com>
2021-03-20 14:30:17 -0500

    database up and working as expected

---
+++ database.sql
@@ -0,0 +1,20 @@
+-- db name: to_do --
+
+CREATE TABLE "tasks"(
+    "id" serial primary key,
+    "newTask" varchar(100),
+    "complete" boolean,
+    "delete" boolean
+);
+
+INSERT INTO "tasks" ( "newTask", complete, delete ) 
+VALUES 
+    ( 'do the dishes', false, false ),
+    ( 'do homework', false, false ),
+    ( 'clean office space', false, false ),
+    ( 'take dog on walk', false, false ),
+    ( 'clean up yard', false, false ),
+    ( 'grocery shopping', false, false ),
+    ( 'target trip', false, false );
+
+SELECT * FROM "tasks";
